467e0d0af2f39705f58aa892b3acee0bffb91c8f
Anthony José <anthonyJ2017@icloud.com>
2021-10-19 10:27:04 -0300

    Initial Commit

---
+++ Shared/mapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapsApp.swift
+//  Shared
+//
+//  Created by Anthony José on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct mapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
